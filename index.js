8fa5d04a0586f0365510b36490ce5151c6f9c7ca
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-03-27 19:56:17 -0300

    read files logic

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express');
+import { throws } from 'assert';
+import express from 'express';
 const app = express();
-const fs = require('fs');
+import { appendFile, read, readFile, write, writeFile } from 'fs';
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hi Johnny');
-})
+let states = [];
+let cities = [];
 
-fs.appendFile('teste.txt', 'hahahah', (err) => {
-  if (err) throw err;
-  console.log('Saved!')
-})
+async function readAndCreate() {
+  await readCities();
+  await readStates();
+  console.log(states);
+  console.log(cities);
+}
+
+async function readCities () {
+  readFile('./jsonFiles/Cidades.json', (err, data) => {
+    if (err) throw err;
+    cities =  JSON.parse(data);
+  });
+}
+
+async function readStates() {
+  readFile('./jsonFiles/Estados.json', (err, data) => {
+    if (err) throw err;
+    states = await JSON.parse(data);
+  });
+}
 
 app.listen(port, () => {
   console.log (`Example app listening at http://localhost:${port}`);
 })
+
+readAndCreate();
